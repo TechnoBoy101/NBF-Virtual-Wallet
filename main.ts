5ae808c36f2830e7645116456858adb2a940b610
TechnoBoy101 <71509553+TechnoBoy101@users.noreply.github.com>
2020-10-13 12:55:33 +0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,33 @@
+let Username = ""
 let Password = ""
-game.splash("NBF Virtual Wallet")
-let Username = game.askForString("Username:")
-if (Username == "1000Infinity") {
-    game.splash("You have, ", "141 gems")
-    game.splash("You are in Silver Category")
-    game.reset()
-} else if (Username == "InvictusAbhi") {
-    Password = game.askForString("Password:")
-    if (Password == "Abhi2010") {
-        game.splash("You have, ", "348 gems")
-        game.splash("You are in Platinum Category")
+let AbhiMoney = 0
+forever(function () {
+    game.splash("NBF Virtual Wallet")
+    Username = game.askForString("Username:")
+    if (Username == "1000Infinity") {
+    	
+    } else if (Username == "InvictusAbhi") {
+        Password = game.askForString("Password:")
+        if (Password == "Abhi2010") {
+            AbhiMoney += game.askForNumber("How much money do you want to add?")
+        } else {
+            game.splash("Incorrect Passcode")
+        }
+    } else if (Username == "NBFMaster101") {
+        Password = game.askForString("Password:")
+        if (Password == "dagger29") {
+            game.splash("You have, ", "165 gems")
+            game.splash("You are in Silver Category")
+            game.reset()
+        } else {
+            game.splash("Incorrect Passcode")
+        }
+    } else if (Username == "InvictusSK") {
+        game.splash("You have, ", "512 gems")
+        game.splash("You are in Diamond Category")
         game.reset()
     } else {
-        game.splash("Incorrect Passcode")
-    }
-} else if (Username == "NBFMaster101") {
-    Password = game.askForString("Password:")
-    if (Password == "dagger29") {
-        game.splash("You have, ", "165 gems")
-        game.splash("You are in Silver Category")
+        game.splash("Incorrect username")
         game.reset()
-    } else {
-        game.splash("Incorrect Passcode")
     }
-} else if (Username == "InvictusSK") {
-    game.splash("You have, ", "512 gems")
-    game.splash("You are in Diamond Category")
-    game.reset()
-} else {
-    game.splash("Incorrect username")
-    game.reset()
-}
+})
